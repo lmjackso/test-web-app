9f34ecfc7e50599e8a20d51b5ec645ad4fb60008
FriskyPanda <larryjiii@hotmail.com>
2013-07-12 11:05:03 -0400

    Initial Blog.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'a6e8ee7f13670fc2b2097725b2903e007d5d8e5239e85ba3fab0fd35613e65a25809b8c3ef9d9520dd97bb20e29af688e2a888244c61b9c4498fa359f7f944f8'
